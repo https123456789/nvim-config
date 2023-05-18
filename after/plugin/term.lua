77a8a5cec6efae7a92f98e2bd5385284b3e6d249
https123456789 <familylandon01@outlook.com>
2023-05-18 16:49:21 -0700

    feat: add keymaps for showing and hiding terminal

--- after/plugin/term.lua
+++ after/plugin/term.lua
@@ -1,12 +1,16 @@
 vim.g.floaterm_titleposition = "center"
 
--- Opening in normal mode
+-- Keymaps for normal mode
+vim.keymap.set("n", "<leader>st", vim.cmd.FloatermShow)
 vim.keymap.set("n", "<leader>tm", vim.cmd.FloatermNew)
 
 -- Keymaps while in the terminal
 vim.keymap.set("t", "<C-t>", vim.cmd.FloatermNew)
 vim.keymap.set("t", "<C-Tab>", vim.cmd.FloatermNext)
 vim.keymap.set("t", "<C-S-Tab>", vim.cmd.FloatermPrev)
+
+-- To hide
+vim.keymap.set("t", "<C-w>", vim.cmd.FloatermHide)
 
 -- To exit
 vim.keymap.set("t", "<C-q>", function()
