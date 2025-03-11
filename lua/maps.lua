05347d414ea6a2e75f999ede529829c4f576a6a2
Ben Landon <landb0x207@gmail.com>
2025-03-11 15:34:05 -0700

    feat: tmux integration

--- lua/maps.lua
+++ lua/maps.lua
@@ -1,10 +1,6 @@
 -- Window management
 vim.keymap.set("n", "<leader>c", "<C-w>c")
 vim.keymap.set("n", "<leader>n", "<C-w>n")
-vim.keymap.set("n", "<C-Left>", "<C-w>H")
-vim.keymap.set("n", "<C-Right>", "<C-w>L")
-vim.keymap.set("n", "<C-Up>", "<C-w>K")
-vim.keymap.set("n", "<C-Down>", "<C-w>J")
 
 -- Tab management
 vim.keymap.set("n", "<leader>to", vim.cmd.tabnew)
